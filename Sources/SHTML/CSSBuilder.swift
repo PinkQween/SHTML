7e9350ce7f193465bd511746d2b7459cbad1cab0
Hanna Skairipa <hanna@hannaskairipa.com>
2026-02-13 09:37:12 -0700

    css colours

--- Sources/SHTML/CSSBuilder.swift
+++ Sources/SHTML/CSSBuilder.swift
@@ -121,12 +121,24 @@ public func background(_ value: String) -> CSSProperty {
     CSSProperty("background", value)
 }
 
+public func background(_ value: Color) -> CSSProperty {
+    CSSProperty("background", value.css)
+}
+
 public func backgroundColor(_ value: String) -> CSSProperty {
     CSSProperty("background-color", value)
 }
 
+public func backgroundColor(_ value: Color) -> CSSProperty {
+    CSSProperty("background-color", value.css)
+}
+
 public func color(_ value: String) -> CSSProperty {
     CSSProperty("color", value)
+}
+
+public func color(_ value: Color) -> CSSProperty {
+    CSSProperty("color", value.css)
 }
 
 // MARK: - Borders & Radius
