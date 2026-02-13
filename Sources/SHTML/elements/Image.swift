d3a8c39292f0e48c722c54a707d3882b08f96325
Hanna Skairipa <hanna@hannaskairipa.com>
2026-02-13 11:29:22 -0700

    fix

--- Sources/SHTML/elements/Image.swift
+++ Sources/SHTML/elements/Image.swift
@@ -1,20 +1,28 @@
 public struct Image: HTMLPrimitive, HTMLModifiable {
     public typealias Body = Never
     
+    public var attributes: [String: String] = [:]
+    public var styles: [String: String] = [:]
+    
     public var src: String
     public var alt: String
     
-    public init(src: ImageName, alt: String = "") {
-        self.src = src.path
+    public init(_ imageName: ImageName, alt: String = "") {
+        self.src = imageName.path
         self.alt = alt
     }
     
     public func render() -> String {
-        let attrs = HTMLRendering.renderAttributes(attributes)
-        return {
-                img(src: src, alt: alt)
+        var attrs = attributes
+        attrs["src"] = src
+        attrs["alt"] = alt
+        
+        if !styles.isEmpty {
+            let styleStr = styles.map { "\($0.key): \($0.value)" }.joined(separator: "; ")
+            attrs["style"] = styleStr
         }
+        
+        let attrsString = HTMLRendering.renderAttributes(attrs)
+        return "<img\(attrsString) />"
     }
 }
-
-public typealias img = Img
